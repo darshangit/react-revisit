d645ea9cd27d785f2fb3c585b6a4d4e4b9789ed0
Darshan Satya <darshans1989@gmail.com>
2021-04-08 20:31:05 -0400

    Using Fragment so that we do not have a additional parent element

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -1,21 +1,25 @@
-import { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Aux from "../../../hoc/aux";
 import cssClasses from "./Person.css";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] render");
 
-    return [
+    return (
+      <Fragment className={cssClasses.Person}>
         <p key="i1" onClick={this.props.click}>
           I'm a {this.props.name} and am {this.props.age} old!
-        </p>,
-        <p key="i2">{this.props.children}</p>,
-        <input key="i3"
+        </p>
+        <p key="i2">{this.props.children}</p>
+        <input
+          key="i3"
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-    ];
+      </Fragment>
+    );
   }
 }
 
